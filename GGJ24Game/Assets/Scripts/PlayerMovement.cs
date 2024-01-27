1c97632e6778c24fff186717c025966737c91959
barkinspmz <barkinsapmaz2@outlook.com>
2024-01-27 03:36:36 +0300

    Player movement | Jump lock.
    There is jump count restriction. It prevents the jumping more than 1.

--- GGJ24Game/Assets/Scripts/PlayerMovement.cs
+++ GGJ24Game/Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private bool _canMove = true;
     [SerializeField] private float _playerMovementSpeed = 5f;
+    [SerializeField] private float jumpForce = 30f;
 
     private Rigidbody2D _rb2D;
 
-    public float horizontalInput;
-    
+    private float horizontalInput;
+
+    [SerializeField] private Transform feetPos = null;
+    [SerializeField] private LayerMask floorMask;
+
+    private bool _canMove = true;
     void Start()
     {
         _rb2D = GetComponent<Rigidbody2D>();
@@ -20,11 +24,17 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space) &&_canMove && Physics2D.OverlapCircle(feetPos.position, 0.3f, floorMask))
+        {
+            _rb2D.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 
     private void FixedUpdate()
     {
         if (_canMove)
-        _rb2D.velocity = new Vector2(horizontalInput*_playerMovementSpeed, _rb2D.velocity.y);
+        {
+            _rb2D.velocity = new Vector2(horizontalInput * _playerMovementSpeed, _rb2D.velocity.y);
+        }
     }
 }
